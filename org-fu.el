3a31f172497d75727009edc31298e08b8166f329
Oleh Krehel <ohwoeowho@gmail.com>
2015-03-17 13:38:47 +0100

    Re-prefix everything to "orfu"

--- org-fu.el
+++ org-fu.el
@@ -8,26 +8,25 @@
 ;; * wiki/stack.org with level 1: Questions
 
 ;;* base directory
-(defun org-expand (name)
+(defun orfu-expand (name)
   (expand-file-name name "~/Dropbox/org"))
 
-(defcustom org-fu-github-project-name
+(defcustom orfu-github-project-name
   "https://github\\.com/abo-abo/\\([^/]+\\)"
   "Regex for Github repository projects."
   :type 'string)
 
 ;;* capture
 ;;** basic
-;; http://orgmode.org/manual/Capture-templates.html#Capture-templates
 (require 'org-capture)
-(setq
- org-capture-templates
- '(("t" "TODO" entry (file+headline (org-expand "gtd.org") "Tasks")
+;; http://orgmode.org/manual/Capture-templates.html#Capture-templates
+(setq org-capture-templates
+      '(("t" "TODO" entry (file+headline (orfu-expand "gtd.org") "Tasks")
     "* TODO %^{Brief Description}\nAdded: %U\n%?\n")
-   ("b" "Buffer" entry (file+headline (org-expand "gtd.org") "Tasks")
+        ("b" "Buffer" entry (file+headline (orfu-expand "gtd.org") "Tasks")
     "* TODO %a")))
 ;;** project
-(defvar org-project-list
+(defvar orfu-project-list
   '(("ELISP" "e" "elisp")
     ("FARGS" "f" "function-args")
     ("WORF" "w" "worf")
@@ -39,20 +38,20 @@
    (add-to-list
     'org-capture-templates
     (destructuring-bind (tag key name) project
-      `(,key ,name entry (file+olp (org-expand "gtd.org") "Projects" ,name)
+      `(,key ,name entry (file+olp (orfu-expand "gtd.org") "Projects" ,name)
              ,(format
                "* TODO %%^{Brief Description}  :%s:\nAdded: %%U  %%i\n  %%?\n"
                tag)
              :clock-in t :clock-resume t))))
- org-project-list)
+ orfu-project-list)
 ;;** PDF
 (push
- '("p" "Pdf article" entry (file+olp (org-expand "gtd.org") "Projects" "Scientific Articles")
-   "* TODO Read %(org-process-current-pdf)%(org-set-tags-to\"OFFICE\")\nAdded: %U %i\n  %?\n")
+ '("p" "Pdf article" entry (file+olp (orfu-expand "gtd.org") "Projects" "Scientific Articles")
+   "* TODO Read %(orfu-process-current-pdf)%(org-set-tags-to\"OFFICE\")\nAdded: %U %i\n  %?\n")
  org-capture-templates)
 
 (require 'org-attach)
-(defun org-process-current-pdf ()
+(defun orfu-process-current-pdf ()
   (let* ((buffer (org-capture-get :buffer))
          (buffer-mode (with-current-buffer buffer major-mode))
          (filename (org-capture-get :original-file)))
@@ -61,7 +60,7 @@
         (setq filename (dired-get-filename))))
     (when (or (string= (file-name-extension filename) "pdf")
               (string= (file-name-extension filename) "djvu"))
-      (let ((org-attach-directory (org-expand "data/"))
+      (let ((org-attach-directory (orfu-expand "data/"))
             (name (file-name-sans-extension
                    (file-name-nondirectory filename))))
         (org-attach-attach filename nil 'cp)
@@ -73,11 +72,11 @@
 ;;** protocol
 (require 'org-protocol)
 (setq org-protocol-default-template-key "l")
-(push '("l" "Link" entry (function org-handle-link)
-        "* TODO %(org-wash-link)\nAdded: %U\n%(org-link-hooks)\n%?")
+(push '("l" "Link" entry (function orfu-handle-link)
+        "* TODO %(orfu-wash-link)\nAdded: %U\n%(orfu-link-hooks)\n%?")
       org-capture-templates)
 
-(defun org-wash-link ()
+(defun orfu-wash-link ()
   "Return a pretty-printed top of `org-stored-links'.
 Try to remove superfluous information, like website title."
   (let ((link (caar org-stored-links))
@@ -86,43 +85,43 @@ Try to remove superfluous information, like website title."
      link
      (replace-regexp-in-string " - Stack Overflow" "" title))))
 
-(defvar org-link-hook nil)
+(defvar orfu-link-hook nil)
 
-(defun org-link-hooks ()
+(defun orfu-link-hooks ()
   (prog1
       (mapconcat #'funcall
-                 org-link-hook
+                 orfu-link-hook
                  "\n")
-    (setq org-link-hook)))
+    (setq orfu-link-hook)))
 
-(defun wmctrl-raise-frame ()
+(defun orfu-raise-frame ()
   (if (eq system-type 'gnu/linux)
       (call-process
        "wmctrl" nil nil nil "-i" "-R"
        (frame-parameter (selected-frame) 'outer-window-id))
     (raise-frame)))
 
-(defun org-handle-link ()
-  (wmctrl-raise-frame)
+(defun orfu-handle-link ()
+  (orfu-raise-frame)
   (let ((link (caar org-stored-links))
         file)
     (cond ((string-match "^https://www.youtube.com/" link)
-           (org-handle-link-youtube link))
+           (orfu-handle-link-youtube link))
           ((string-match (regexp-quote "http://stackoverflow.com/") link)
-           (find-file (org-expand "wiki/stack.org"))
+           (find-file (orfu-expand "wiki/stack.org"))
            (goto-char (point-min))
            (re-search-forward "^\\*+ +Questions" nil t))
-          ((string-match org-fu-github-project-name link)
-           (find-file (org-expand (format "wiki/%s.org" (match-string 1 link))))
+          ((string-match orfu-github-project-name link)
+           (find-file (orfu-expand (format "wiki/%s.org" (match-string 1 link))))
            (goto-char (point-min))
            (re-search-forward "^\\*+ +Issues" nil t))
           (t
-           (find-file (org-expand "ent.org"))
+           (find-file (orfu-expand "ent.org"))
            (goto-char (point-min))
            (re-search-forward "^\\*+ +Articles" nil t)))))
 
 (require 'async)
-(defun org-handle-link-youtube (link)
+(defun orfu-handle-link-youtube (link)
   (lexical-let*
       ((file-name (org-trim
                    (shell-command-to-string
@@ -134,7 +133,7 @@ Try to remove superfluous information, like website title."
        (dir "~/Downloads/Videos")
        (full-name
         (expand-file-name file-name dir)))
-    (add-hook 'org-link-hook
+    (add-hook 'orfu-link-hook
               (lambda ()
                 (concat
                  (org-make-link-string dir dir)
@@ -142,7 +141,7 @@ Try to remove superfluous information, like website title."
                  (org-make-link-string full-name file-name))))
     (async-shell-command
      (format "youtube-dl \"%s\" -o \"%s\"" link full-name))
-    (find-file (org-expand "ent.org"))
+    (find-file (orfu-expand "ent.org"))
     (goto-char (point-min))
     (re-search-forward "^\\*+ +Videos" nil t)))
 
